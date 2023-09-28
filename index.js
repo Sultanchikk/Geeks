1a6190b988c8b7d020d2a6170d7278ad587aad47
Sultanchikk <98090790+Sultanchikk@users.noreply.github.com>
2023-09-28 18:25:37 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// console.log("start");
+
+// function func() {
+//   setTimeout(() => {
+//     console.log("Timeout done");
+//   }, 2000);
+//   console.log("function finish");
+// }
+
+// console.log("object");
+// func();
+// console.log("end");
+
+// function fn1() {
+//   console.log("First msg");
+// }
+
+// function fn2() {
+//   console.log("Second msg");
+// }
+
+// function fn3() {
+//   console.log("Third msg");
+// }
+
+// function callAllMsgs() {
+//   fn1();
+//   setTimeout(fn2, 1000);
+//   fn3();
+// }
+
+// callAllMsgs();
